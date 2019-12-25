44e84c716ac8477e18047c87d06af7eb56f11c89
Aleksey Iskovskikh <iskovskikh56@gmail.com>
2019-12-25 19:08:58 +0500

    Доработка 2

--- app/src/main/java/com/example/inputform/MainActivity.java
+++ app/src/main/java/com/example/inputform/MainActivity.java
@@ -9,7 +9,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txt1;
+    private EditText edt;
+    private EditText edt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-private TextView txt1;
-private EditText edt;
-private EditText edt1;
+
     private void Init()
     {
         Button btnClear = findViewById(R.id.buttonClear);
@@ -39,11 +39,11 @@ private EditText edt1;
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String stringEdt1 = getString(R.string.txtinf1)+edt1.getText().toString();
-                String stringEdt = getString(R.string.txtinf2)+edt.getText().toString();
-                String res = stringEdt1 + stringEdt;
-                txt1.setText(res);
-
+                String text = getString(R.string.txtFull, edt1.getText().toString(), edt.getText().toString());
+//                String stringEdt1 = getString(R.string.txtinf1)+edt1.getText().toString();
+//                String stringEdt = getString(R.string.txtinf2)+edt.getText().toString();
+//                String res = stringEdt1 + stringEdt;
+                txt1.setText(text);
 
             }
         });
